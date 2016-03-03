e73a501c859d9b9a279ee016e8b4a5b23f1c24c4
Max Hantke <maxhantke@gmail.com>
2016-03-03 15:05:47 +0100

    version number corrected in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='h5writer',
-      version='0.1.3',
+      version='0.1.4',
       description='Writing HDF5 files with openMPI.',
       author='Max F. Hantke, Benedikt Daurer',
       author_email='maxhantke@gmail.com',
