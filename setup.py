9ad63a084474b27a5674fae81376c0a5fa889088
Max Felix Hantke <maxhantke@gmail.com>
2019-02-21 16:29:12 +0000

    add license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='h5writer',
-      version='0.5.12',
+      version='0.5.13',
       description='Tool for writing HDF5 files',
       author='Max F. Hantke, Benedikt Daurer, Filipe R. N. C. Maia',
       author_email='maxhantke@gmail.com',
@@ -11,5 +11,6 @@ setup(name='h5writer',
       install_requires=['h5py>=2.2'],
       extras_require={'mpi': 'mpi4py>=1.3.1'},
       packages = ['h5writer'],
+      license='MIT License',
 )
 
