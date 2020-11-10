866504b5f5b88124b496aa1d60cda2392cd4480d
Jamie van der Sanden <jamie.sanden@refeyn.com>
2020-11-10 17:23:47 +0000

    Create `__version__.py` module and use it in `setup.py`

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+import re
 from setuptools import setup
+
+NAME_PACKAGE = "lw8"
+
+PATH_CWD = Path()
+PATH_PACKAGE = PATH_CWD / NAME_PACKAGE
+PATH_VERSION = PATH_PACKAGE / "_version.py"
+
+
+def get_version_tag(path_version: Path) -> str:
+    verstrline = open(str(path_version), "rt").read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo is not None:
+        return mo.group(1)
+    else:
+        raise RuntimeError(f"Unable to find version string in {str(path_version)}")
+
 
 setup(
     name="h5writer",
-    version="0.7.0",
+    version=get_version_tag(PATH_VERSION),
     description="Tool for writing HDF5 files",
     author="Max F. Hantke, Benedikt Daurer, Filipe R. N. C. Maia, Jamie van der Sanden",
     author_email="maxhantke@gmail.com",
